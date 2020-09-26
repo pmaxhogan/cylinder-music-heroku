60ff9f057d32e0da478e5cc76f78141c9ac420e3
realprogrammer5000 <me@programmer5000.com>
2020-09-25 22:18:47 -0400

    Remove old api

--- index.js
+++ index.js
@@ -38,31 +38,3 @@ app.get('/api/getAlbums', async (request, response) => {
     const results = await getAllOfSomething("https://amp-api.music.apple.com/v1/me/library/albums?include[library-albums]=artists&include[library-artists]=catalog&fields[artists]=url&includeOnly=catalog%2Cartists&limit=100", request.query.auth, request.query.token);
     response.json(results);
 })
-
-app.get('/api/imgProxy', async (request, response) => {
-    let isInvalid = false;
-    if(!request.query.url || typeof request.query.url !== "string"){
-        isInvalid = true;
-    }
-    try{
-        const url = new URL(request.query.url.toString());
-        if(!url.hostname.endsWith(".mzstatic.com")){
-            isInvalid = true;
-        }
-    }catch (e) {
-        isInvalid = true;
-    }
-
-    if(isInvalid){
-        return response.status(400).end();
-    }
-
-
-    const resp = await fetch(request.query.url);
-    response.header("Content-Type", resp.headers.get("content-type"))
-    response.header("cache-control", resp.headers.get("cache-control"))
-    response.header("etag", resp.headers.get("etag"))
-    response.header("content-length", resp.headers.get("content-length"))
-    response.header("last-modified", resp.headers.get("last-modified"))
-    resp.body.pipe(response);
-})
